daa2fa0eb63ffcc5c876f816002a825d31914fbc
LaraGuardiola <webdev.slara@gmail.com>
2021-07-11 15:48:01 +0200

    Añadida posibilidad de escoger tamaño de parrilla y numero de bombas al comenzar un juego, añadido contador de posiciones exploradas al final del juego, reducido el uso de variables(con funciones duplicadas), simplificado el numero de llamadas en main

--- Buscaminas/Buscaminas.java
+++ Buscaminas/Buscaminas.java
@@ -1,70 +1,74 @@
 package Buscaminas;
 
-import java.time.LocalDate;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Buscaminas {
 
     //Ajustes del juego
-    private final static int size = 5;
-    private final static int bombs = 3;
+    private static int size;
+    private static int bombs;
 
     //Mapa del usuario y del ordenador
-    private static char[][] userMap = new char[size][size];
-    private static char[][] computerMap = new char[size][size];
+    private static char[][] userMap;
+    private static char[][] computerMap;
 
     //Posiciones
-    private final static char unexplored = 'X';
-    private final static char explored = 'O';
-    private final static char bomb = 'B';
+    private final static char UNEXPLORED = 'X';
+    private final static char EXPLORED = 'O';
+    private final static char BOMB = 'B';
 
     //Scanner
     private static Scanner sc;
 
     //Coordenadas y contador de bombas autogeneradas
     private static String fila, columna;
-    private static int bombCounter = 0;
     private static int positionsExplored = 0;
 
     public static void main(String[] args) {
         intro();
-        bombHasBeenPlanted();
         generateMap();
         checkPosition();
     }
 
     private static void intro(){
+        sc = new Scanner(System.in);
         System.out.println("************ BUSCAMINAS ************\n");
+        System.out.println("1. Escoge el tamaño de la parrilla de juego");
+        size = sc.nextInt();
+        System.out.println("2. Selecciona el número de bombas");
+        bombs = sc.nextInt();
+        bombHasBeenPlanted(size,bombs);
     }
 
-    private static void bombHasBeenPlanted(){
-        
+    private static void bombHasBeenPlanted(int size, int bombs){
+        computerMap = new char[size][size];
         //Genera el mapa del ordenador en funcion de los ajustes globales / primero declara todo como inexplorado y luego coloca bombas al azar
-
+        System.out.println(size + bombs);
         for(int i = 0; i < computerMap.length;i++){
             for(int j = 0; j < computerMap[0].length; j++){
-                computerMap[i][j] = unexplored;
+                computerMap[i][j] = UNEXPLORED;
             }
         }
+        System.out.println(size + bombs);
         plantTheBomb();
     }
 
     private static void plantTheBomb(){
+        System.out.println(size + bombs);
         for(int z = 0; z < bombs; z++){
-            computerMap[randomNumber(size)][randomNumber(size)] = bomb;
-            bombCounter++;
+            computerMap[randomNumber(size)][randomNumber(size)] = BOMB;
         }
     }
 
     private static void generateMap(){ //genera el mapa de usuario y lo imprime en consola
-
+        userMap = new char[size][size];
         for(int i = 0; i < userMap.length;i++){
             for(int j = 0; j < userMap[0].length; j++){
-                userMap[i][j] = unexplored;
+                userMap[i][j] = UNEXPLORED;
                 System.out.print(userMap[i][j] + "\t");
             }
-            System.out.println(""); 
+            System.out.println("\n"); 
         }
     }
 
@@ -91,18 +95,18 @@ public class Buscaminas {
     }
 
     private static void checkCoordinates(String row, String column){
-        int x = stringParser(row);
+        int x = stringParser(row); //parseamos string a int
         int y = stringParser(column);
-        if(computerMap[x][y] == unexplored){
-            userMap[x][y] = explored;
+        if(computerMap[x][y] == UNEXPLORED){
+            userMap[x][y] = EXPLORED;
             positionsExplored++;
             areYaWinningSon();
             rerender(x, y);
             checkPosition();
-        }else if(computerMap[x][y] == bomb){
-            userMap[x][y] = bomb;
+        }else if(computerMap[x][y] == BOMB){
+            userMap[x][y] = BOMB;
             dyingRerender(x, y);
-            System.out.println("HAS PERDIDO");
+            System.out.printf("¡BOOM! HAS PERDIDO. Has explorado un total de %d posiciones. Suerte la próxima",positionsExplored);
             System.exit(0);
         }
     }
@@ -115,29 +119,29 @@ public class Buscaminas {
     }
 
     private static void areYaWinningSon(){
-        if(positionsExplored == Math.pow(size, 2) - bombCounter){
+        if(positionsExplored == Math.pow(size, 2) - bombs){
             System.out.println("Primera vez que ganas en tu vida al buscaminas");
             System.exit(0);
         }
     }
 
     private static void rerender(int row, int column) {
-        userMap[row][column] = explored;
+        userMap[row][column] = EXPLORED;
         for(int i = 0; i < userMap.length;i++){
             for(int j = 0; j < userMap[0].length; j++){
                 System.out.print(userMap[i][j] + "\t");
             }
-            System.out.println(""); //salto de linea por cada fila
+            System.out.println("\n"); //salto de linea por cada fila
         }
     }
 
     private static void dyingRerender(int row, int column) {
-        userMap[row][column] = bomb;
+        userMap[row][column] = BOMB;
         for(int i = 0; i < userMap.length;i++){
             for(int j = 0; j < userMap[0].length; j++){
                 System.out.print(userMap[i][j] + "\t");
             }
-            System.out.println(""); //salto de linea por cada fila
+            System.out.println("\n"); //salto de linea por cada fila
         }
     }
 
